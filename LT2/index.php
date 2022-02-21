4d7da8c7ce75ec25e1ea7c960c356e486f062c2c
Maurich-Gellaco <99902448+Maurich-Gellaco@users.noreply.github.com>
2022-02-21 15:05:03 +0800

    Create index.php

---
+++ LT2/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		My First Website
+	</title>
+</head>
+<body>
+<h1> Hello, I am Ma.Yna Maurich Gellaco, this is my first web page. </h1>
+<ul>
+	<li><a href = "index.php"> HOME</a></li>
+	<li><a href = "/LT2/index.html"> LEARNING TASK</a></li>
+</ul>
+</body>
+</html>
